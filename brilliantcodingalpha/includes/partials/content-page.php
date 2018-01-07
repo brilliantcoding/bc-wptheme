04c2c28a60454e2a7ffa63029a3282a38f473778
Matthew Jackowski <mattjjacko+github@gmail.com>
2018-01-07 13:44:18 -0800

    removing disqus from pages

--- brilliantcodingalpha/includes/partials/content-page.php
+++ brilliantcodingalpha/includes/partials/content-page.php
@@ -72,29 +72,3 @@
     </article>
   </div>
 </div>
-
-<!-- Disqus setup script -->
-<div id="disqus_thread"></div>
-<script>
-    /**
-     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
-     */
-     
-     var disqus_config = function () {
-      this.page.url = '<?php echo $post['permalink']; ?>';
-      this.page.identifier = '<?php echo $post['name']; ?>';
-      this.page.title = '<?php echo $post['title']; ?>';
-    };
-    
-    (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
-      var d = document, s = d.createElement('script');
-      
-        s.src = '//<?php  echo $data['theme']['mod_disquisshortname']; ?>.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
-        
-        s.setAttribute('data-timestamp', + new Date());
-        (d.head || d.body).appendChild(s);
-      })();
-    </script>
-    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
-
